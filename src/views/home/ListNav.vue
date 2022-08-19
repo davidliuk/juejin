8d2f60f15f8cf277930ff614e3212a7d5dda338b
gk-chang <zhikai_chang@163.com>
2022-08-19 12:03:20 +0800

    新建列表导航栏文件

---
+++ src/views/home/ListNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="list-header" style="display:;">
+    <nav role="navigation" class="list-nav">
+        <ul class="nav-list left">
+          <li class="nav-item active">
+            <a href="/">推荐</a>
+          </li>
+          <li class="nav-item">
+            <a href="/?sort=newest">最新</a>
+          </li>
+          <li class="nav-item">
+            <a href="/?sort=three_days_hottest">热榜</a>
+          </li>
+        </ul>
+        <div class="dorp-down-area">
+        <!---->
+        </div>
+    </nav>
+  </header>
+</template>
+    
+<script lang='ts'>
+import { defineComponent } from 'vue'
+    
+export default defineComponent({
+  setup() {
+    // todo
+    return {}
+  }
+})
+</script>
+    
+<style lang='stylus' scoped></style>
+    
