13bff58a61cc6b2ff319bb21cbf049c5f482a24b
zaizai <327120052@qq.com>
2022-08-19 11:46:35 +0800

    20220819 qianlinyi 完成侧边栏开发（下滑侧边栏悬浮置顶未实现）

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,12 +1,18 @@
 <template>
   <nav>顶部通栏综合等一直吸附在顶部</nav>
-  <AppTopNav />
-  <header>头部掘金图标</header>
-  <h1>test</h1>
-  <main>
+  <AppTopNav/>
+<!--  <header>头部掘金图标</header>-->
+<!--  <h1>test</h1>-->
+  <main class="main-content">
     <!-- 二级路由 -->
-    <router-view></router-view>
+    <div class="left-column">
+      <router-view></router-view>
+    </div>
     <!-- <Header/> -->
+    <div class="right-column">
+      <AppSideBar></AppSideBar>
+      <!-- <Catalog :catalog-data="catalogData"></Catalog> -->
+    </div>
   </main>
   <!-- <footer>底部</footer> -->
 </template>
@@ -14,6 +20,8 @@
 <script>
 // import Header from "@/components/Header.vue"
 import AppTopNav from '@/components/AppTopNav.vue'
+import AppSideBar from "@/components/AppSideBar.vue";
+
 export default {
   name: 'Layout',
   components: {
@@ -22,9 +30,23 @@ export default {
 }
 </script>
 
-<style >
+<style scoped lang="less">
 
-:root {
-  font-size: 12px;
+.main-content {
+  position: relative;
+  width: 960px;
+  margin: 100px auto;
+
+  .left-column {
+    width: 700px;
+  }
+
+  .right-column {
+    position: absolute;
+    width: 240px;
+    top: 0;
+    right: 0;
+  }
+
 }
 </style>
