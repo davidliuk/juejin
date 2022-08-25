4a7c7f3934739b52daf72b38318fc1afe7b5bc35
zaizai <327120052@qq.com>
2022-08-25 15:38:22 +0800

    文章列表页面优化

--- src/views/post/ShowPost.vue
+++ src/views/post/ShowPost.vue
@@ -1,16 +1,20 @@
 <template>
-  <h1>文章列表</h1>
-  <main class="container">
-    <table class="table-striped">
+  <main class="container mt-5">
+    <div class="row page-header justify-content-center align-items-center">
+      <h1>文章列表</h1>
+    </div>
+    <table class="table table-striped mt-3">
       <thead>
-      <tr>
+      <tr class="text-center">
         <th>编号</th>
         <th>标题</th>
         <th>分类</th>
         <th>正文</th>
         <th>选项</th>
       </tr>
-      <tr v-for="(val,key) in data.post">
+      </thead>
+      <tbody>
+      <tr v-for="(val,key) in data.post" class="text-center">
         <td>
           {{ key }}
         </td>
@@ -24,12 +28,13 @@
           {{ val[2].substring(0, 25) + '...' }}
         </td>
         <td>
-          <form class="inline" :action="'/api/user/post/'+key.toString()+'/delete'" method="post" target="_blank">
-            <button onclick="return confirm('确认删除？')">删除</button>
+          <form class="inline" :action="'/api/user/post/'+key.toString()+'/delete'" method="post"
+                @submit.prevent="articleDelete(key)">
+            <button class="btn btn-danger btn-md" onclick="return confirm('确认删除？')">删除</button>
           </form>
         </td>
       </tr>
-      </thead>
+      </tbody>
     </table>
   </main>
 </template>
@@ -54,45 +59,63 @@ export default {
           this.data = response.data
         });
   },
+  methods: {
+    articleDelete(index) {
+      axios
+          .post('http://127.0.0.1:5000/user/post/'+index.toString()+'/delete')
+          .then((response) => {
+            console.log(response)
+            console.log(response.data.status)
+            if (response.data.status === 'failed') {
+              alert('');
+            } else {
+              alert('文章删除成功！')
+              //sessionStorage.setItem('userInfo', JSON.stringify(res.data))
+              this.$router.go(0)
+            }
+          })
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
-.container {
-  display: flex;
-  flex-direction: column;
-
-  table {
-    width: 100%;
-    max-width: 100%;
-    margin-bottom: 1rem;
-    background-color: transparent;
-
-    th, td {
-      padding: 0.75rem;
-      vertical-align: top;
-      border-top: 1px solid #dee2e6;
-
-      a {
-        color: #213547;
-      }
-
-      a:hover {
-        color: #fff;
-      }
-    }
-
-    thead th {
-      vertical-align: bottom;
-      border-bottom: 2px solid #dee2e6;
-    }
-  }
-
-  .table-striped {
-    tr:nth-of-type(odd) {
-      background-color: rgba(0, 0, 0, 0.05);
-    }
-  }
-
-}
+@import "../../assets/theme.css";
+//.container {
+//  display: flex;
+//  flex-direction: column;
+//
+//  table {
+//    width: 100%;
+//    max-width: 100%;
+//    margin-bottom: 1rem;
+//    background-color: transparent;
+//
+//    th, td {
+//      padding: 0.75rem;
+//      vertical-align: top;
+//      border-top: 1px solid #dee2e6;
+//
+//      a {
+//        color: #213547;
+//      }
+//
+//      a:hover {
+//        color: #fff;
+//      }
+//    }
+//
+//    thead th {
+//      vertical-align: bottom;
+//      border-bottom: 2px solid #dee2e6;
+//    }
+//  }
+//
+//  .table-striped {
+//    tr:nth-of-type(odd) {
+//      background-color: rgba(0, 0, 0, 0.05);
+//    }
+//  }
+//
+//}
 </style>
